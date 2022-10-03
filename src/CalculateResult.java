643f4d4e66a9a74a78eb8281ddc50df867c895f0
Indigo Bosworth <indigobosworth@gmail.com>
2022-10-03 11:36:55 +0100

    Created and Init-ed Calculate Result class

---
+++ src/CalculateResult.java
@@ -0,0 +1,21 @@
+/*************************************************************************
+ *  Class for evaluating valid input
+ *  works as the calculator part of the program
+ *
+ *  @author	 Indigo Bosworth
+ *           Sai Pranay Gamini
+ *  @Creation Date: 3/10/2022
+ *          
+ *
+ *************************************************************************/
+
+
+public class CalculateResult {
+    
+    //Stub method to return result of a valid user input
+    //Part of Task #5 on Github
+    public static int RunCalculator(String validInput){
+
+        return 0;
+    }
+}
