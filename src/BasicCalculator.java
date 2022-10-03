e2c8ab7636b58e90795d29b36a43ddc63b9a4444
Indigo Bosworth <indigobosworth@gmail.com>
2022-10-03 11:01:03 +0100

    Project Files Initialised

---
+++ src/BasicCalculator.java
@@ -0,0 +1,19 @@
+/*************************************************************************
+ *  Basic Calculator Main class.
+ *  Currently unfinished
+ *
+ *  @version [FILL IN]
+ *
+ *  @author [FILL IN]
+ *          
+ *
+ *************************************************************************/
+
+
+public class BasicCalculator {
+
+	public static void main(String[] args) {
+
+  }
+
+}
