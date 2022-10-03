f876f46b90f1216d2d12fbf60ff46040ac1e1152
Indigo Bosworth <indigobosworth@gmail.com>
2022-10-03 11:40:10 +0100

    Gave description to main file

--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -1,6 +1,7 @@
 /*************************************************************************
  *  Basic Calculator Main class.
- *  Currently unfinished
+ *  Runs in loop of getting user input, checking if it is valid
+ *  Then if it is valid, printing the result of the calculation to the user
  *
  *  @author	 Indigo Bosworth
  *           Sai Pranay Gamini
@@ -12,8 +13,10 @@
 
 public class BasicCalculator {
 
+  //Main method for program, does as required by assignment description (see above)
+  //Part of Task #6 on Github
 	public static void main(String[] args) {
-
+    
   }
 
 }
