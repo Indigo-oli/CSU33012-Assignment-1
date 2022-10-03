666c9dcf1c4bcc25afc1728934517a5833d769d0
saipranayg22 <75270185+saipranayg22@users.noreply.github.com>
2022-10-03 20:18:47 +0100

    Revert "All files initial"

--- README.txt
+++
@@ -1,7 +0,0 @@
-Initial README file
-
-To be dealt with as part of Issue 1 and Issue 2 on Github
-
-[Add Project description here]
-
-[Add Project report here]
--- src/BasicCalculator.java
+++ src/BasicCalculator.java
@@ -1,11 +1,10 @@
 /*************************************************************************
  *  Basic Calculator Main class.
- *  Runs in loop of getting user input, checking if it is valid
- *  Then if it is valid, printing the result of the calculation to the user
+ *  Currently unfinished
  *
- *  @author	 Indigo Bosworth
- *           Sai Pranay Gamini
- *  @Creation Date: 3/10/2022
+ *  @version [FILL IN]
+ *
+ *  @author [FILL IN]
  *          
  *
  *************************************************************************/
@@ -13,10 +12,8 @@
 
 public class BasicCalculator {
 
-  //Main method for program, does as required by assignment description (see above)
-  //Part of Task #6 on Github
 	public static void main(String[] args) {
-    
+
   }
 
 }
