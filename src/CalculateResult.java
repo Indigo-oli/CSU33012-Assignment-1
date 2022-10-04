60208d458ecae75a42e3b9a07dd1fd79daafcbdd
Indigo Bosworth <indigobosworth@gmail.com>
2022-10-04 13:36:50 +0100

    Added initial RunCalculator code

--- src/CalculateResult.java
+++ src/CalculateResult.java
@@ -16,6 +16,29 @@ public class CalculateResult {
     //Part of Task #5 on Github
     public static int RunCalculator(String validInput){
 
-        return 0;
+        //error checking for if valid input is null
+        
+        //This method assumes that order of operations need not be obeyed
+
+        //method needs to go through string, and at each equation calculate results based on two sides
+        String[] equationNumbers = validInput.split("\\+|\\*|-");   //get all numbers involved first
+        String   operatorsInUse = validInput.replaceAll("\\d",""); //then get all operators involved
+
+        int result = Integer.parseInt(equationNumbers[0]);
+        int operatorCount = 0;
+        for(int i = 1; i < equationNumbers.length; i++){
+            switch(operatorsInUse.charAt(operatorCount)){
+                case '*':
+                    result *= Integer.parseInt(equationNumbers[i]);
+                case '+':
+                    result += Integer.parseInt(equationNumbers[i]);
+                case '-':
+                    result -= Integer.parseInt(equationNumbers[i]);
+                default:
+            }
+            operatorCount++;
+        }
+
+        return result;
     }
 }
